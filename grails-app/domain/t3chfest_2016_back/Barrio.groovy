122b94a081c5ba50c29bd8dd2a7a9d752b1db74b
rafbermudez <rafa@rafbermudez.com>
2016-02-06 11:53:52 +0100

    Clases de dominio iniciales

---
+++ grails-app/domain/t3chfest_2016_back/Barrio.groovy
@@ -0,0 +1,10 @@
+package t3chfest_2016_back
+
+class Barrio {
+    
+    Long id
+    String nombre
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/t3chfest_2016_back/HistoricoPorBarrio.groovy
@@ -0,0 +1,12 @@
+package t3chfest_2016_back
+
+class HistoricoPorBarrio {
+    
+    Long id
+    Integer mes
+    Integer year
+    Barrio barrio
+
+    static constraints = {
+    }
+}
