7f4526733258792ff06000ceaafcb752edcff480
rafbermudez <rafa@rafbermudez.com>
2016-02-06 12:09:56 +0100

    Actualizado gitignore

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import t3chfest_2016_back.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        
+        //Creación de los barrios
+        
+        
     }
     def destroy = {
     }
