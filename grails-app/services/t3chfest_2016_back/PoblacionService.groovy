5cb67b990d3427baa1257f4f79c0f62763627ad3
pablo_yump <pablo.gonzalez@yump.com>
2016-02-06 12:25:56 +0100

    Primer intento leer csv poblacion

--- grails-app/services/t3chfest_2016_back/PoblacionService.groovy
+++ grails-app/services/t3chfest_2016_back/PoblacionService.groovy
@@ -2,10 +2,33 @@ package t3chfest_2016_back
 
 import grails.transaction.Transactional
 
+import org.grails.plugins.csv.CSVMapReader
+
 @Transactional
 class PoblacionService {
 
-    def serviceMethod() {
+    def leerPoblacionPorMes()
+    {
+        File defaultDir = new File("data")
+        String fileName = "ene15.csv"
+        
+        CSVMapReader reader = new CSVMapReader(new File(dir, fileName).newReader())
+        
+        reader.each{
+            csvLineMap ->
+            
+            def barrio = Barrio.findByName(csvLineMap.Distrito)
+            def historico = HistoricoPorBarrio.findByBarrioAndMesAndAno(barrio,csvLineMap.Mes,csvLineMap.Ano)
 
+            historico.poblacion = csvLineMap.Total
+            
+            if (!historico.save()) {
+                historico.errors.each {
+                    println it
+                }
+            }
+
+        }
     }
+
 }
